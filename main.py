89db02d9350b86dde06315687b2124f66b902f62
Googlefan <google@neody.dev>
2024-12-12 10:50:31 +0000

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Set, Union
+from typing import Dict, Optional, Set
 from google.genai.chats import Chat
 import google.genai as genai
 import google.genai.types as gtypes
@@ -11,6 +11,7 @@ from time import time
 import os
 from dotenv import load_dotenv
 import requests
+from threading import Thread
 
 load_dotenv()
 
@@ -25,17 +26,18 @@ class Sink(voice_recv.AudioSink):
         self.speaking: Set[int] = set()
         self.last_sent = 0.0
         self.chat = chat
-        self.audios: Dict[int, AudioSegment] = {}
+        self.audios: Dict[int, bytes] = {}
+        self.play_queue = []
 
     def send(self, parts):
         response = self.chat.send_message(parts)
         for part in response.candidates[0].content.parts:
+            if part.text.startswith("think:"):
+                print(part.text.capitalize())
+                continue
             self.play(part.text)
 
     def play(self, content: str):
-        if content.startswith("thinking:"):
-            print(content.capitalize())
-            return
         print(f"Speak: {content}")
         res = requests.post(
             "http://localhost:5004/synthesize",
@@ -43,9 +45,47 @@ class Sink(voice_recv.AudioSink):
         )
         if self.voice_client is None:
             return
-        self.voice_client.play(
-            discord.player.FFmpegPCMAudio(BytesIO(res.content), pipe=True)
-        )
+        aus = discord.player.FFmpegPCMAudio(BytesIO(res.content), pipe=True)
+        self.play_queue.append(aus)
+        if len(self.play_queue) == 1:
+            self._play()
+
+    def _play(self):
+        def next_p(x):
+            self.play_queue.pop(0)
+            self._play()
+
+        if len(self.play_queue) > 0:
+            first = self.play_queue[0]
+            self.voice_client.play(first, after=next_p)
+
+    def do_chat(self):
+        with BytesIO() as io:
+            merged_audio = None
+            for a in self.audios.values():
+                a = AudioSegment(a, sample_width=4, frame_rate=48000, channels=1)
+                a = a + 2
+                if merged_audio is None:
+                    merged_audio = a
+                else:
+                    merged_audio += a
+            self.audios = {}
+            if merged_audio is None or merged_audio.duration_seconds < 1.0:
+                return
+            merged_audio.export("out.wav", format="wav")
+            merged_audio.export(io, format="wav")
+            io.seek(0)
+            self.send(
+                [
+                    gtypes.Part.from_uri(
+                        genaiold.upload_file(io, mime_type="audio/wav").uri,
+                        "audio/wav",
+                    ),
+                    gtypes.Part.from_text(
+                        "[SYSTEM MESSAGE] Follow the system instruction. Don't forget you should respond in the language the user speaks."
+                    ),
+                ]
+            )
 
     @voice_recv.AudioSink.listener()
     def on_voice_member_speaking_stop(self, member: discord.Member):
@@ -53,34 +93,14 @@ class Sink(voice_recv.AudioSink):
             return
         if member.id in self.speaking:
             self.speaking.remove(member.id)
+        now = time()
         if (
             len(self.speaking) == 0
-            and self.last_sent + 7.0 < time()
+            and self.last_sent + 5.0 < now
             and self.audios is not None
         ):
-            self.last_sent = time()
-            with BytesIO() as io:
-                merged_audio: List[AudioSegment] = []
-                for a in self.audios.values():
-                    merged_audio.extend(a)
-                merged_audio: Union[AudioSegment, int] = sum(merged_audio)
-                if isinstance(merged_audio, int) or merged_audio.duration_seconds < 2.0:
-                    return
-                self.audios = {}
-                merged_audio.export("out.wav", format="wav")
-                merged_audio.export(io, format="wav")
-                io.seek(0)
-                self.send(
-                    [
-                        gtypes.Part.from_uri(
-                            genaiold.upload_file(io, mime_type="audio/wav").uri,
-                            "audio/wav",
-                        ),
-                        gtypes.Part.from_text(
-                            "[SYSTEM MESSAGE] Follow the system instruction. Don't forget you should respond in the language the user speaks."
-                        ),
-                    ]
-                )
+            self.last_sent = now
+            Thread(target=self.do_chat).start()
 
     @voice_recv.AudioSink.listener()
     def on_voice_member_speaking_start(self, member: discord.Member):
@@ -92,12 +112,11 @@ class Sink(voice_recv.AudioSink):
         return False
 
     def write(self, user: Optional[discord.User], data: voice_recv.VoiceData) -> None:
-        s = AudioSegment(data.pcm, sample_width=4, frame_rate=48000, channels=1)
         if user is not None:
             if user.id in self.audios:
-                self.audios[user.id] += s
+                self.audios[user.id] += data.pcm
             else:
-                self.audios[user.id] = s
+                self.audios[user.id] = data.pcm
 
     def cleanup(self) -> None:
         self.chat = None
@@ -111,7 +130,10 @@ async def on_voice_member_disconnect(member: discord.Member, ssrc: int | None):
     sink = sinks.get(member.voice.channel.id)
     if sink is None:
         return
-    sink.send(f"[SYSTEM MESSAGE] {member.display_name} left the voice channel")
+    Thread(
+        target=sink.send,
+        args=(f"[SYSTEM MESSAGE] {member.display_name} left the voice channel",),
+    ).start()
 
 
 @bot.event
@@ -119,7 +141,10 @@ async def on_voice_member_platform(member: discord.Member, platform: int | str |
     sink = sinks.get(member.voice.channel.id)
     if sink is None:
         return
-    sink.send(f"[SYSTEM MESSAGE] {member.display_name} joined the voice channel")
+    Thread(
+        target=sink.send,
+        args=(f"[SYSTEM MESSAGE] {member.display_name} joined the voice channel",),
+    ).start()
 
 
 @bot.command()
@@ -134,13 +159,11 @@ async def join(ctx: commands.Context):
             temperature=1.0,
             top_p=0.95,
             top_k=40.0,
-            max_output_tokens=512,
+            max_output_tokens=100,
             response_mime_type="text/plain",
-            system_instruction="""[SYSTEM MESSAGE] You are chatting with user on voice channel.
-You don't have to respond to user everytime. When you think you don't have to speak something, start your response with
-thinking: 
-otherwise, respond in the normal way.""",
-            automatic_function_calling=None,
+            system_instruction="""[SYSTEM MESSAGE] You are chatting with user on voice channel.""",
+            automatic_function_calling=gtypes.AutomaticFunctionCallingConfigDict(),
+            tools=[gtypes.Tool(google_search=gtypes.GoogleSearch())],
         ),
     )
     members = ",".join(
@@ -148,12 +171,18 @@ otherwise, respond in the normal way.""",
     )
     sink = Sink(chat_session)
     vc.listen(sink)
-    sink.send(
-        f"""[SYSTEM MESSAGE] This voice channel has the following members: {members}.
-Currently the voice mode only supports Japanese. Please start by introducing yourself in Japanese."""
-    )
+    Thread(
+        target=sink.send,
+        args=[
+            f"""[SYSTEM MESSAGE] This voice channel has the following members: {members}.
+Currently the voice mode only supports Japanese. Please start by introducing yourself in Japanese.
+Don't do long response. Output your response very short.
+If you think you don't have to respond(like when user isn't actually speaking, but their noise are on the conversation), start your response with `think:`
+あなたはずんだもんです。そのため、語尾にのだを付ける傾向があります。"""
+        ],
+    ).start()
     sinks[voice.channel.id] = sink
-    ctx.reply("準備が完了しました。")
+    await ctx.reply("準備が完了しました。")
 
 
 @bot.command()
